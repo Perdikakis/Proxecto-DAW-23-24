ff833bdc3bc98dbc914b7893c9136b592a2961ed
Perdikakis <jrrdlc@hotmail.com>
2024-11-21 21:19:48 +0100

    tienda

--- frontend/src/componentes/CardCamiseta.jsx
+++ frontend/src/componentes/CardCamiseta.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import '../css/CardCamiseta.css';
 
 const CardCamiseta = ({ id }) => {
+  const [starIcon, setStarIcon] = useState("/icons/star.svg");
+
+  const clickFavoritos = () => {
+    setStarIcon(prevIcon => prevIcon === "/icons/star.svg" ? "/icons/star2.svg" : "/icons/star.svg");
+  };
+
   return (
     <div className="card-camiseta">
       <figure>
         <img src="/img/fondos/destacadas/barcelona.webp" alt="" className="camiseta-img" />
       </figure>
-      <img src="/icons/star.svg" alt="" className="icon-top-left"/>
+      <img 
+        src={starIcon} 
+        alt="Star" 
+        className="icon-top-left" 
+        onClick={clickFavoritos} 
+      />
       <img src="/icons/addCart.svg" alt="" className="icon-top-right"/>
       
       <div className="info-container">
