102db81ee15127ac625691f43cedaaff9284dbd7
Manuel Cordo <jrrdlc@hotmail.com>
2024-11-26 12:42:59 +0100

    cors

--- api/app/Http/Middleware/CORSMiddleware.php
+++ api/app/Http/Middleware/CORSMiddleware.php
@@ -15,23 +15,28 @@ class CORSMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Verificar si la solicitud es OPTIONS
-        if ($request->getMethod() === "OPTIONS") {
-            return response()->json([], 200, [
-                'Access-Control-Allow-Origin' => '*',
-                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
-            ]);
-        }
+        $allowedOrigins = [
+            'https://shop.kakis.es',
+            'https://kakis.es',
+        ];
 
-        $response = $next($request);
+        if (in_array($origin, $allowedOrigins)) {
+            if ($request->getMethod() === "OPTIONS") {
+                return response()->json([], 200, [
+                    'Access-Control-Allow-Origin' => '*',
+                    'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+                ]);
+            }
 
-        // Agregar los encabezados CORS a la respuesta
-        //$response->headers->set('Access-Control-Allow-Origin', 'https://camikakis.es');
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response = $next($request);
+
+            $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+            return $response;
+        }
         return $response;
     }
 }
